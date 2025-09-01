7860e08667718ee139cb302f2e6f630faa0b0f93
Alejo Morell Bethencourt <alejo.morellbethencourt@colaborador.grupoveci.es>
2025-09-01 12:59:04 +0200

    feat: more sense

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-nodeps=$1
 
 if ! [[ -d ".venv"  ]]; then
     python3 -m venv .venv
@@ -8,8 +7,8 @@ fi
 source .venv/bin/activate
 export PYTHONWARNINGS="ignore" # setuptools >81 warn
 
-echo "[INFO] to skip deps installation use: ./start.sh nodeps"
-if [ "$nodeps" != "nodeps" ]; then
+echo "[INFO] pass withdeps to install dependencies from requirements.txt"
+if [ "$1" == "withdeps" ]; then
     pip -r requirements.txt > /dev/null
 fi
 
